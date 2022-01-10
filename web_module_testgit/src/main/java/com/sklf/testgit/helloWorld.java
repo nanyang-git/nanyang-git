591bbf9b4356ba3ec6e5ce73cd82d8d99a57d82d
nanyang <949734042@qq.com>
2022-01-10 14:56:53 +0800

    创建git命令测试模块

---
+++ web_module_testgit/src/main/java/com/sklf/testgit/helloWorld.java
@@ -0,0 +1,8 @@
+package com.sklf.testgit;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("test01");
+        System.out.println("test02222");
+    }
+}
---
+++ web_module_testgit/src/main/java/com/sklf/testgit/helloWorld_yyoj.java
@@ -0,0 +1,13 @@
+package com.sklf.testgit;
+
+public class helloWorld_yyoj {
+    public static void main(String[] args) {
+        System.out.println("test01");
+        System.out.printf("test02");
+        System.out.printf("test03");
+        System.out.printf("test06");
+        System.out.println("test08");
+        System.out.printf("test09");
+
+    }
+}
